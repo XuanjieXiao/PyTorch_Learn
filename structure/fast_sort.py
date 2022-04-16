8dbce2d7e69c6e4ace12d37ac3dde72964d375df
xuanjiexiao <xuanjiexiao@163.com>
2022-04-16 21:53:57 +0800

    update quicksort

--- structure/fast_sort.py
+++ structure/fast_sort.py
@@ -11,20 +11,30 @@
 @time: 2022/4/16 下午8:18
 """
 
-def fast_sort(li,left,right):
+
+def partition(li, left, right):
     temp = li[left]
     while left < right:
         while left < right and li[right] >= temp:
-            right -=1
+            right -= 1
         li[left] = li[right]
 
-        while li[left] <= temp and left<right:
-            left +=1
+        while li[left] <= temp and left < right:
+            left += 1
         li[right] = li[left]
-
     li[left] = temp
+    return left
 
-li = [5,7,4,6,3,1,2,9,8]
+
+def quick_sort(li, left, right):
+    if left < right:
+        mid = partition(li, left, right)
+        quick_sort(li, left, mid - 1)
+        quick_sort(li, mid + 1, right)
+
+
+
+li = [5, 7, 4, 6, 3, 1, 2, 9, 8,4]
 print(li)
-fast_sort(li,0,len(li)-1)
+quick_sort(li, 0, len(li) - 1)
 print(li)
