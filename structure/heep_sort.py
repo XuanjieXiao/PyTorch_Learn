da6504de234189eb835c5d222f504adc54b4c698
xuanjiexiao <xuanjiexiao@163.com>
2022-04-21 10:55:58 +0800

    update heepsort

--- structure/heep_sort.py
+++ structure/heep_sort.py
@@ -11,5 +11,21 @@
 @time: 2022/4/20 下午7:50
 """
 
-if __name__ == "__main__":
-    print()
+def sift(li,low,high):
+    """
+
+    :param li: 列表
+    :param low: 堆的根节点位置
+    :param high: 堆的最后一个元素的位置
+    :return:
+    """
+    i = low #
+    j = 2*i+1 #
+    tmp = li[low] #
+    while j <=high and : #
+        if li[j+1] >li[j]: #
+            j =j+1 #
+
+
+
+
