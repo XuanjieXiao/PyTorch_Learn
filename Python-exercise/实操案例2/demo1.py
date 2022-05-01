d3310f1da3113788abd997d267af89819e51be91
xuanjiexiao <xuanjiexiao@163.com>
2022-05-01 22:06:28 +0800

    more ok

---
+++ Python-exercise/实操案例2/demo1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+"""
+@version: v1.0
+@author: XuanjieXiao
+@license: BSD Licence 
+@contact: xuanjiexiao@163.com
+@site: https://blog.csdn.net/weixin_40749043
+@software: PyCharm
+@file: 的莫.py
+@time: 2022/5/1 下午7:43
+"""
+book_name = 'Java程序设计教程'
+publish = '西安的一个出版社'
+pub_date = '2019-02-02'
+price = 25.56
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
