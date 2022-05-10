69bcf1daa89aa96a33f3d225bd5d8350f5a21193
xuanjiexiao <xuanjiexiao@163.com>
2022-05-10 21:28:06 +0800

    RNN demo3

---
+++ pytorch-test/RNN/RNN_Demo3.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+"""
+@version: v1.0
+@author: XuanjieXiao
+@license: BSD Licence 
+@contact: xuanjiexiao@163.com
+@site: https://blog.csdn.net/weixin_40749043
+@software: PyCharm
+@file: RNN_Demo3.py
+@time: 2022/5/10 下午3:51
+"""
+
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
