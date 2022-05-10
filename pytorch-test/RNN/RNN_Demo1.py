9eb7338cb70d92bf3a57b8916016d20dcb314dd6
xuanjiexiao <xuanjiexiao@163.com>
2022-05-10 14:26:34 +0800

    RNN demo

--- pytorch-test/RNN/RNN_Demo1.py
+++ pytorch-test/RNN/RNN_Demo1.py
@@ -10,6 +10,15 @@
 @file: RNN_Demo1.py
 @time: 2022/5/9 下午8:57
 """
+import torch.nn as nn
+import torch
+
+rnn = nn.RNN(input_size=100,hidden_size=20,num_layers=1)
+print(rnn)
+x=torch.randn(10,3,100)
+out , h =rnn(x,torch.zeros(1,3,20))
+print(out.shape,h.shape)
+
 
 
 def main():
