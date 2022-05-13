0b665fad236ec8d2df688e66ea360e37724b1c88
xuanjiexiao <xuanjiexiao@163.com>
2022-05-13 19:51:50 +0800

    RNN demo3

---
+++ pytorch-test/LSTM/LSTM_Demo1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+"""
+@version: v1.0
+@author: XuanjieXiao
+@license: BSD Licence 
+@contact: xuanjiexiao@163.com
+@site: https://blog.csdn.net/weixin_40749043
+@software: PyCharm
+@file: LSTM_Demo1.py
+@time: 2022/5/13 上午10:08
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
