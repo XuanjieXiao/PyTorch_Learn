3cd49c4d3e1246ef9853323416847965a29e4310
xuanjiexiao <xuanjiexiao@163.com>
2022-05-18 19:48:14 +0800

    Lenet

---
+++ pytorch-test/LeNet-5/LeNet5_Demo.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+"""
+@version: v1.0
+@author: XuanjieXiao
+@license: BSD Licence 
+@contact: xuanjiexiao@163.com
+@site: https://blog.csdn.net/weixin_40749043
+@software: PyCharm
+@file: LeNet5_Demo.py
+@time: 2022/5/18 下午7:46
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
