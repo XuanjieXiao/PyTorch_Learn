50922dd51dffbfe81ca5ba7bc860b0b272375eca
xuanjiexiao <xuanjiexiao@163.com>
2022-05-20 18:43:29 +0800

    000

---
+++ structure/Structure/double_linklist.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+"""
+@version: v1.0
+@author: XuanjieXiao
+@license: BSD Licence 
+@contact: xuanjiexiao@163.com
+@site: https://blog.csdn.net/weixin_40749043
+@software: PyCharm
+@file: double_linklist.py
+@time: 2022/5/19 14:20
+"""
+
+if __name__ == "__main__":
+    print()
---
+++ structure/Structure/radix_sort.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+"""
+@version: v1.0
+@author: XuanjieXiao
+@license: BSD Licence 
+@contact: xuanjiexiao@163.com
+@site: https://blog.csdn.net/weixin_40749043
+@software: PyCharm
+@file: radix_sort.py
+@time: 2022/5/19 8:36
+"""
+
+
+def radix_sort(li):
+    max_num = max(li)
+    it = 0
+    while 10 ** it <= max_num:
+        buckets = [[] for _ in range(10)]
+        for val in li:
+            digit = (val // 10 ** it) % 10
+            buckets[digit].append(val)
+        li.clear
+        for buc in buckets:
+            li.extend(buc)
+        it += 1
+    return li
+
+
+if __name__ == "__main__":
+    print()
