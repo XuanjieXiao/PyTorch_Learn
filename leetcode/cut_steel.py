b4673f6753908a9189cd8bc14771f5d440377ab5
xuanjiexiao <xuanjiexiao@163.com>
2022-05-21 16:35:28 +0800

    steel

--- leetcode/cut_steel.py
+++ leetcode/cut_steel.py
@@ -11,8 +11,41 @@
 @time: 2022/5/21 下午2:48
 """
 
+p = [0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
 
 
+def cut_steel_recurision(p, n):
+    if n == 0:
+        return 0
+    else:
+        res = p[n]
+        for i in range(1, n):
+            res = max(res, cut_steel_recurision(p, i) + cut_steel_recurision(p, i - 1))
+        return res
+
+
+def cut_steel_recurision2(p, n):
+    if n == 0:
+        return 0
+    else:
+        res = 0
+        for i in range(1, n + 1):
+            res = max(res, p[i] + cut_steel_recurision2(p, n - i))
+        return res
+
+
+def cut_steel_dp(p,n):
+    r = [0]
+    for i in range(1,n+1):
+        res = 0
+        for j in range(1,i+1):
+            res = max(res,p[j]+r[i-j])
+        r.append(res)
+    return r[n]
+
+
+
+print(cut_steel_recurision2(p, 8))
 
 
 def main():
