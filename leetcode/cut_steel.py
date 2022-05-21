a756841f759bb81a5105abad9374d1a1151368be
xuanjiexiao <xuanjiexiao@163.com>
2022-05-21 15:28:08 +0800

    ok

---
+++ leetcode/cut_steel.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+"""
+@version: v1.0
+@author: XuanjieXiao
+@license: BSD Licence 
+@contact: xuanjiexiao@163.com
+@site: https://blog.csdn.net/weixin_40749043
+@software: PyCharm
+@file: cut_steel.py
+@time: 2022/5/21 下午2:48
+"""
+
+
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
